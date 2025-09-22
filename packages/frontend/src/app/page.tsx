22d917cc462f8a14f87b25eaefcd86fc35beff29
Guilherme <guilherme.s.barros@outlook.com>
2025-09-22 18:57:17 -0300

    feat: alter login route to /sign-in

--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -1,38 +1,3 @@
-import Link from 'next/link'
-
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-
 export default function Home() {
-	return (
-		<main className="flex h-screen items-center justify-center">
-			<div className="space-y-8">
-				<div className="text-center space-y-1">
-					<h1 className="text-2xl font-bold">Acessar conta</h1>
-					<p className="text-muted-foreground">
-						Para entrar no sistema, é necessário se autenticar.
-					</p>
-				</div>
-
-				<form className="flex flex-col space-y-4">
-					<div className="space-y-2">
-						<Label>Seu e-mail</Label>
-						<Input type="email" placeholder="Informe seu e-mail" />
-					</div>
-
-					<div className="space-y-2">
-						<Label>Sua senha</Label>
-						<Input type="password" placeholder="Informe sua senha" />
-					</div>
-
-					<Button type="submit">Continuar</Button>
-
-					<Button variant="link" asChild>
-						<Link href="/">Não possuo uma conta</Link>
-					</Button>
-				</form>
-			</div>
-		</main>
-	)
+	return <h1>Welcome</h1>
 }
---
+++ packages/frontend/src/app/sign-in/page.tsx
@@ -0,0 +1,44 @@
+import Link from 'next/link'
+
+import type { Metadata } from 'next'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+export const metadata: Metadata = {
+	title: 'Acessar conta',
+}
+
+export default function SignIn() {
+	return (
+		<main className="font-sans flex h-screen items-center justify-center">
+			<div className="flex flex-col items-center gap-8 p-4">
+				<div className="text-center space-y-1">
+					<h1 className="text-2xl font-bold">Acessar conta</h1>
+					<p className="text-muted-foreground">
+						Para entrar na aplicação, é necessário se autenticar.
+					</p>
+				</div>
+
+				<form className="flex w-full flex-col space-y-4">
+					<div className="space-y-2">
+						<Label>Seu e-mail</Label>
+						<Input type="email" placeholder="Informe seu e-mail" />
+					</div>
+
+					<div className="space-y-2">
+						<Label>Sua senha</Label>
+						<Input type="password" placeholder="Informe sua senha" />
+					</div>
+
+					<Button type="submit">Continuar</Button>
+
+					<Button variant="link" asChild>
+						<Link href="#visual-only-link">Não possuo uma conta</Link>
+					</Button>
+				</form>
+			</div>
+		</main>
+	)
+}
