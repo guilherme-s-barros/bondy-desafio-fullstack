186064ae9a1cdd1a6114b0206c9b2196ee46de55
Guilherme <guilherme.s.barros@outlook.com>
2025-09-25 20:57:34 -0300

    feat: change logo based on theme

--- packages/frontend/src/app/(auth)/layout.tsx
+++ packages/frontend/src/app/(auth)/layout.tsx
@@ -1,11 +1,12 @@
-import Image from 'next/image'
+import { ThemeImage } from '@/components/theme-image'
 
 export default function Layout({ children }: React.PropsWithChildren) {
 	return (
 		<div className="flex h-screen bg-[url('/bg.png')] dark:bg-[url('/bg-dark.png')]">
 			<div className="flex flex-1 items-center justify-center">
-				<Image
-					src="/logo.svg"
+				<ThemeImage
+					srcLight="/logo.svg"
+					srcDark="/logo-dark.svg"
 					alt="Bondy Logo"
 					height={192}
 					width={390}
