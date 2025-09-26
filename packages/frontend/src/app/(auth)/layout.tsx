985f96f2f7d0cabe288bbd8aef6bec698cd5f5c5
Guilherme <guilherme.s.barros@outlook.com>
2025-09-25 23:29:30 -0300

    feat: make auth layout responsive

--- packages/frontend/src/app/(auth)/layout.tsx
+++ packages/frontend/src/app/(auth)/layout.tsx
@@ -3,18 +3,21 @@ import { ThemeImage } from '@/components/theme-image'
 export default function Layout({ children }: React.PropsWithChildren) {
 	return (
 		<div className="flex h-screen bg-[url('/bg.png')] dark:bg-[url('/bg-dark.png')]">
-			<div className="flex flex-1 items-center justify-center">
+			<div className="max-md:hidden flex flex-1 items-center justify-center">
 				<ThemeImage
 					srcLight="/logo.svg"
 					srcDark="/logo-dark.svg"
 					alt="Bondy Logo"
 					height={192}
 					width={390}
+					style={{ minWidth: 390 }}
 					unoptimized
 				/>
 			</div>
 
-			<div className="w-[624px] bg-muted">{children}</div>
+			<div className="w-full max-w-[624px] max-md:max-w-full bg-muted">
+				{children}
+			</div>
 		</div>
 	)
 }
